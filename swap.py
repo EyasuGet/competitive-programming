f573b76522f6a9e0e04f2c16ed3f633bc26ebe16
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2024-08-09 16:40:48 +0300

    Add files via upload

---
+++ swap.py
@@ -0,0 +1,13 @@
+def minimumOperations(self, nums):
+        count=0
+        for i in nums:
+            if i % 3 == 0:
+                continue
+            elif (i % 3 == 1):
+                count += 1
+            elif ((i % 3)+1) % 3 == 0
+                count += 1
+            else:
+                continue
+        return count
+print(minimumOperations)

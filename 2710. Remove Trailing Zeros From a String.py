8ea9c4146fc7e913a59bf12eb97f212a54a30830
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2024-09-21 23:23:58 +0300

    Add files via upload

---
+++ 2710. Remove Trailing Zeros From a String.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def removeTrailingZeros(self, num):
+        """
+        :type num: str
+        :rtype: str
+        """
+        l = 0
+        r = len(num) - 1
+
+        while l < r:
+            if num[l] == "0":
+                l += 1
+            elif num[r] == "0":
+                r -= 1
+            else:
+                return num[l:r+1]
+        return num[l:r+1]
+
+        # return num.rstrip('0')
+        
+        

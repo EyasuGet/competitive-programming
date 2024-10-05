7a5b5f977b55594dc2cf58f6a9a5bc7bdb7b73af
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2024-10-05 12:40:02 +0300

    Time: 20 ms (61.73%) | Memory: 11.9 MB (38.74%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def permute(self, nums):
+        ans , sol = [] , []
+        n = len(nums)
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])
+            
+            for i in nums:
+                if i not in sol:
+                    sol.append(i)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans
+        #tc = o(n!) 
+        #sc = o(n)

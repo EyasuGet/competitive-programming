9cfb2e567d0542bdfe5bc435db618b496be686a5
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-01-30 18:25:09 +0300

    Created 'Convert the Temperature' by A2SV Hub

---
+++ 30-Jan-2025/Convert the Temperature 227249.py
@@ -0,0 +1,8 @@
+# Problem: Convert the Temperature - https://leetcode.com/problems/convert-the-temperature/
+
+array=[]
+fahr = celsius + 273.15
+kev = celsius*1.80 + 32.00
+array.append(fahr)
+array.append(kev)
+return array      

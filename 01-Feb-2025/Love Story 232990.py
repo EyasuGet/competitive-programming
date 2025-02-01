849bea6a32f448870daa692f80224e0ff8667b45
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-02-01 04:10:55 +0300

    Created 'Love Story' by A2SV Hub

---
+++ 01-Feb-2025/Love Story 232990.py
@@ -0,0 +1,15 @@
+# Problem: Love Story - https://codeforces.com/contest/1829/problem/A
+
+t = int(input())
+for _ in range(t):
+    str1 = input()
+    s = "codeforces"
+    
+    p = 0
+    count = 0
+    while p < 10:
+        if str1[p] != s[p]:
+            count += 1
+        p += 1
+    print(count)
+    

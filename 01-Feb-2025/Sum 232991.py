e0f039aa2e9b30b217a2b14da366eb6710b7886c
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-02-01 04:14:23 +0300

    Created 'Sum' by A2SV Hub

---
+++ 01-Feb-2025/Sum 232991.py
@@ -0,0 +1,10 @@
+# Problem: Sum - https://codeforces.com/contest/1742/problem/A
+
+t = int(input())
+for _ in range(t):
+    nums = sorted(map(int, input().split()))
+    if nums[-1] == nums[0] + nums[1]:
+        print("YES")
+    else:
+        print("NO")
+    

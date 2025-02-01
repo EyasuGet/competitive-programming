bd16e95ab46ecc0ac3d4ea75c322d37c291ed0c4
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-02-01 21:47:28 +0300

    Time: 4 ms (14.09%) | Memory: 12.8 MB (23.49%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def getConcatenation(self, nums):
+        return nums + nums
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        # for i in range(len(nums)):
+        #     nums.append(nums[i])
+        # return nums
+        return nums + nums
+        

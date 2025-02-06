e2bfb82ea529380fcfa10ae0c85eb3d17380f540
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-02-06 08:44:20 +0300

    Created 'Majority Element II (Optional)' by A2SV Hub

---
+++ 06-Feb-2025/Majority Element II (Optional) 245594.py
@@ -0,0 +1,15 @@
+# Problem: Majority Element II (Optional) - https://leetcode.com/problems/majority-element-ii/
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        count = int(len(nums) / 3)
+
+        ans = []
+        freq = Counter(nums)
+
+        for key,value in freq.items():
+            if value > count:
+                ans.append(key)
+        
+
+        return ans

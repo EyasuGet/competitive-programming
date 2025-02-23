34aba2743d1371b5caaec78c5fd486ce596f3b69
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-02-23 09:14:10 +0300

    Created 'Car Pooling' by A2SV Hub

---
+++ 23-Feb-2025/Car Pooling 280693.py
@@ -0,0 +1,19 @@
+# Problem: Car Pooling - https://leetcode.com/problems/car-pooling/description/
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        
+        
+        count = [0] * 1001
+
+        for trip in trips:
+            passanger, start, end = trip
+
+            count[start] += passanger
+            count[end] -= passanger
+        for i in range(1, len(count)):
+            count[i] += count[i-1]
+        if max(count) > capacity:
+            return False
+        else:
+            return True

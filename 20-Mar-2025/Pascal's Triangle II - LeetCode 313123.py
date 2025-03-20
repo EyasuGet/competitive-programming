a48c0a72acc72f53e4bdb9e8cf1a445f4d432446
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-03-20 14:24:19 +0300

    Created 'Pascal's Triangle II - LeetCode' by A2SV Hub

---
+++ 20-Mar-2025/Pascal's Triangle II - LeetCode 313123.py
@@ -0,0 +1,28 @@
+# Problem: Pascal's Triangle II - LeetCode - https://leetcode.com/problems/pascals-triangle-ii/
+
+class Solution:
+    def getRow(self, n: int) -> List[int]:
+        
+        if n == 0:
+            return [1]
+        
+        prev = self.getRow(n-1)
+        row = []
+        for i in range(n//2+1):
+            if i == 0:
+                row.append(1)
+            else:
+                row.append(prev[i] + prev[i-1])
+        
+        ans = []
+        if n % 2 != 0:
+            ans = row + row[::-1]
+        else:
+            ans = row + row[:-1][::-1]
+
+        return ans
+        
+
+
+
+            

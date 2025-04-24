3b8e1a822ce750c4b9caba594893feb6663d0b7f
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-04-25 01:14:17 +0300

    Created 'Permutations' by A2SV Hub

---
+++ 24-Apr-2025/Permutations 341994.py
@@ -0,0 +1,16 @@
+# Problem: Permutations - https://leetcode.com/problems/permutations/
+
+class Solution(object):
+    def permute(self, nums):
+        ans , sol = [] , []
+        n = len(nums)
+        def backtrack():
+            if len(sol) == n:
+                ans.append(sol[:])  
+            for i in nums:
+                if i not in sol:
+                    sol.append(i)
+                    backtrack()
+                    sol.pop()
+        backtrack()
+        return ans

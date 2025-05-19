b79b6b776f2ce64603d61db41b78f31af6b0a190
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-05-19 22:29:09 +0300

    Created 'Find All Duplicates in an Array' by A2SV Hub

---
+++ 19-May-2025/Find All Duplicates in an Array 358166.py
@@ -0,0 +1,12 @@
+# Problem: Find All Duplicates in an Array - https://leetcode.com/problems/find-all-duplicates-in-an-array/description/
+
+class Solution(object):
+    def findDuplicates(self, nums):
+        hashmap = {}
+        dup = []
+        for i in nums:
+            if i in hashmap:
+                dup.append(i)
+            else:
+                hashmap[i] = 1
+        return dup

961fb1cc766b1dc642abd39fcf5f95a76d40a057
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-05-25 23:55:57 +0300

    Time: 76 ms (85.71%) | Memory: 24.6 MB (86.29%) - LeetSync

---
+++ 456-132-pattern/132-pattern.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def find132pattern(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        third_value = float('-inf')
+        stack = []
+
+        for number in reversed(nums):
+            if number < third_value:
+                return True
+
+            while stack and stack[-1] < number:
+ 
+                third_value = stack.pop()
+
+            stack.append(number)
+
+        return False
+        

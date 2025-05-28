e469dbd5894015e43fe0caa26b704eebaaa86f7d
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-05-28 10:54:49 +0300

    Time: 43 ms (72.80%) | Memory: 18 MB (91.13%) - LeetSync

---
+++ 2347-count-nodes-equal-to-average-of-subtree/count-nodes-equal-to-average-of-subtree.py
@@ -0,0 +1,23 @@
+class Solution:
+    def __init__(self):
+        self.matchingSubtreeCount = 0
+
+    def calculateSubtreeValues(self, currentNode):
+        if currentNode is None:
+            return 0, 0  
+
+        leftSubtree  = self.calculateSubtreeValues(currentNode.left)
+        rightSubtree = self.calculateSubtreeValues(currentNode.right)
+
+        sumOfValues  = leftSubtree [0] + rightSubtree[0] + currentNode.val
+        numberOfNodes  = leftSubtree [1] + rightSubtree[1] + 1
+
+        if sumOfValues  // numberOfNodes  == currentNode.val:
+            self.matchingSubtreeCount += 1
+
+        return sumOfValues , numberOfNodes
+
+
+    def averageOfSubtree(self, root):
+        self.calculateSubtreeValues(root)
+        return self.matchingSubtreeCount 

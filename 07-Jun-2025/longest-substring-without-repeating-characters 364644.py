bd1596223130116920830aa76b7205ff0435315d
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-06-08 00:00:34 +0300

    Created 'longest-substring-without-repeating-characters' by A2SV Hub

---
+++ 07-Jun-2025/longest-substring-without-repeating-characters 364644.py
@@ -0,0 +1,14 @@
+# Problem: longest-substring-without-repeating-characters - https://leetcode.com/problems/longest-substring-without-repeating-characters/
+
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        check = set()
+        left = 0
+        longest = 0
+        for right in range(len(s)):
+            while s[right] in check:
+                check.remove(s[left]) 
+                left += 1
+            check.add(s[right])
+            longest = max(longest, right-left + 1)
+        return longest

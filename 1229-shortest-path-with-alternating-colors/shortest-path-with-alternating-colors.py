7d1fed3af262fd74d8c065f97e16e05cb19906e6
Eyasu Getaneh <161371246+EyasuGet@users.noreply.github.com>
2025-06-11 22:28:15 +0300

    Time: 2 ms (81.06%) | Memory: 17.9 MB (71.31%) - LeetSync

---
+++ 1229-shortest-path-with-alternating-colors/shortest-path-with-alternating-colors.py
@@ -0,0 +1,37 @@
+class Solution(object):
+    def shortestAlternatingPaths(self, n, redEdges, blueEdges):
+        """
+        :type n: int
+        :type redEdges: List[List[int]]
+        :type blueEdges: List[List[int]]
+        :rtype: List[int]
+        """
+        RED, BLUE = 0, 1
+
+        graph = [defaultdict(list), defaultdict(list)]
+        for u, v in redEdges:
+            graph[RED][u].append(v)
+        for u, v in blueEdges:
+            graph[BLUE][u].append(v)
+
+        res = [-1] * n
+        visited = [[False] * n for _ in range(2)]  
+
+        q = deque()
+        q.append((0, 0, -1))  
+
+        while q:
+            node, dist, prev_color = q.popleft()
+
+            if res[node] == -1:
+                res[node] = dist
+
+            for color in [RED, BLUE]:
+                if color == prev_color:
+                    continue  
+                for nei in graph[color][node]:
+                    if not visited[color][nei]:
+                        visited[color][nei] = True
+                        q.append((nei, dist + 1, color))
+
+        return res
